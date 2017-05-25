62c89731e0c71b296018ca39b13ebce4bd646ac6
mohammeddahhou <simohammed.dahhou@gmail.com>
2017-05-26 00:21:10 +0200

    add signin and signup activities

--- app/src/main/java/com/example/mohammeddahhou/eroller/MainActivity.java
+++ app/src/main/java/com/example/mohammeddahhou/eroller/MainActivity.java
@@ -1,13 +1,31 @@
 package com.example.mohammeddahhou.eroller;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    private TextView signupLink;
+    private static final int REQUEST_SIGNUP = 0;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        signupLink = (TextView) findViewById(R.id.link_signup);
+
+        signupLink.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View v) {
+                // Start the Signup activity
+                Intent intent = new Intent(getApplicationContext(), SignUp.class);
+                startActivityForResult(intent, REQUEST_SIGNUP);
+            }
+        });
     }
 }
